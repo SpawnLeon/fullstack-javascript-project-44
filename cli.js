401fc57e827b94d76d95852618e45e438fbb521f
Alexander Leonov <spawnleon@gmail.com>
2023-08-12 12:22:22 +0400

    feat: add eslint

--- cli.js
+++ cli.js
@@ -4,5 +4,5 @@ const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = rl.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-}
+};
 export default greeting;
